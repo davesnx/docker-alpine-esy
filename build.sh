e56dfa79221feba09c7a0c6b49353d6b885bdb07
David Sancho Moreno <dsnxmoreno@gmail.com>
2022-11-29 18:57:40 +0100

    Fix dockerfile

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ VERSION=dev
 DOCKER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Building $IMAGE:$VERSION image..."
-docker build -f "$DOCKER_DIR"/Dockerfile -t $IMAGE:$VERSION "$DOCKER_DIR" --platform linux/amd64
+docker build -f "$DOCKER_DIR"/Dockerfile -t $IMAGE:$VERSION "$DOCKER_DIR" --platform linux/amd64 --progress plain
 echo ""
 
 echo "Done."
