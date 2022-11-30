9da678d3e8c6d54561f1148518db206cea50ab6d
David Sancho Moreno <dsnxmoreno@gmail.com>
2022-11-30 02:23:00 +0100

    Stuff

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 set -o pipefail
 
-IMAGE=davesnx/alpine-esy
+IMAGE=davesnx/docker-alpine-esy
 VERSION=dev
 
 DOCKER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -12,4 +12,4 @@ echo "Building $IMAGE:$VERSION image..."
 docker build -f "$DOCKER_DIR"/Dockerfile -t $IMAGE:$VERSION "$DOCKER_DIR" --platform linux/amd64 --progress plain
 echo ""
 
-echo "Done."
+echo "Done"
